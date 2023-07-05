58912161793ef2405a95277fec8a9f3817911377
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2023-07-05 13:20:01 +0300

    Update generated swagger client code
    Signed-off-by: Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>

--- client/repositories/repositories_client.go
+++ client/repositories/repositories_client.go
@@ -28,34 +28,48 @@ type ClientOption func(*runtime.ClientOperation)
 
 // ClientService is the interface for Client methods
 type ClientService interface {
-	Create(params *CreateParams, opts ...ClientOption) (*CreateOK, error)
+	CreateRepo(params *CreateRepoParams, opts ...ClientOption) (*CreateRepoOK, error)
 
-	Delete(params *DeleteParams, opts ...ClientOption) error
+	CreateRepoPool(params *CreateRepoPoolParams, opts ...ClientOption) (*CreateRepoPoolOK, error)
 
-	Get(params *GetParams, opts ...ClientOption) (*GetOK, error)
+	DeleteRepo(params *DeleteRepoParams, opts ...ClientOption) error
 
-	List(params *ListParams, opts ...ClientOption) (*ListOK, error)
+	DeleteRepoPool(params *DeleteRepoPoolParams, opts ...ClientOption) error
+
+	GetRepo(params *GetRepoParams, opts ...ClientOption) (*GetRepoOK, error)
+
+	GetRepoPool(params *GetRepoPoolParams, opts ...ClientOption) (*GetRepoPoolOK, error)
+
+	ListRepoInstances(params *ListRepoInstancesParams, opts ...ClientOption) (*ListRepoInstancesOK, error)
+
+	ListRepoPools(params *ListRepoPoolsParams, opts ...ClientOption) (*ListRepoPoolsOK, error)
+
+	ListRepos(params *ListReposParams, opts ...ClientOption) (*ListReposOK, error)
+
+	UpdateRepo(params *UpdateRepoParams, opts ...ClientOption) (*UpdateRepoOK, error)
+
+	UpdateRepoPool(params *UpdateRepoPoolParams, opts ...ClientOption) (*UpdateRepoPoolOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
 }
 
 /*
-Create creates repository with the parameters given
+CreateRepo creates repository with the parameters given
 */
-func (a *Client) Create(params *CreateParams, opts ...ClientOption) (*CreateOK, error) {
+func (a *Client) CreateRepo(params *CreateRepoParams, opts ...ClientOption) (*CreateRepoOK, error) {
 	// TODO: Validate the params before sending
 	if params == nil {
-		params = NewCreateParams()
+		params = NewCreateRepoParams()
 	}
 	op := &runtime.ClientOperation{
-		ID:                 "Create",
+		ID:                 "CreateRepo",
 		Method:             "POST",
 		PathPattern:        "/repositories",
 		ProducesMediaTypes: []string{"application/json"},
 		ConsumesMediaTypes: []string{"application/json"},
 		Schemes:            []string{"http"},
 		Params:             params,
-		Reader:             &CreateReader{formats: a.formats},
+		Reader:             &CreateRepoReader{formats: a.formats},
 		Context:            params.Context,
 		Client:             params.HTTPClient,
 	}
@@ -67,32 +81,69 @@ func (a *Client) Create(params *CreateParams, opts ...ClientOption) (*CreateOK, 
 	if err != nil {
 		return nil, err
 	}
-	success, ok := result.(*CreateOK)
+	success, ok := result.(*CreateRepoOK)
 	if ok {
 		return success, nil
 	}
 	// unexpected success response
-	unexpectedSuccess := result.(*CreateDefault)
+	unexpectedSuccess := result.(*CreateRepoDefault)
 	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
 /*
-Delete deletes repository by ID
+CreateRepoPool creates repository pool with the parameters given
 */
-func (a *Client) Delete(params *DeleteParams, opts ...ClientOption) error {
+func (a *Client) CreateRepoPool(params *CreateRepoPoolParams, opts ...ClientOption) (*CreateRepoPoolOK, error) {
 	// TODO: Validate the params before sending
 	if params == nil {
-		params = NewDeleteParams()
+		params = NewCreateRepoPoolParams()
 	}
 	op := &runtime.ClientOperation{
-		ID:                 "Delete",
+		ID:                 "CreateRepoPool",
+		Method:             "POST",
+		PathPattern:        "/repositories/{repoID}/pools",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &CreateRepoPoolReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*CreateRepoPoolOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*CreateRepoPoolDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+DeleteRepo deletes repository by ID
+*/
+func (a *Client) DeleteRepo(params *DeleteRepoParams, opts ...ClientOption) error {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewDeleteRepoParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "DeleteRepo",
 		Method:             "DELETE",
 		PathPattern:        "/repositories/{repoID}",
 		ProducesMediaTypes: []string{"application/json"},
 		ConsumesMediaTypes: []string{"application/json"},
 		Schemes:            []string{"http"},
 		Params:             params,
-		Reader:             &DeleteReader{formats: a.formats},
+		Reader:             &DeleteRepoReader{formats: a.formats},
 		Context:            params.Context,
 		Client:             params.HTTPClient,
 	}
@@ -108,22 +159,53 @@ func (a *Client) Delete(params *DeleteParams, opts ...ClientOption) error {
 }
 
 /*
-Get gets repository by ID
+DeleteRepoPool deletes repository pool by ID
 */
-func (a *Client) Get(params *GetParams, opts ...ClientOption) (*GetOK, error) {
+func (a *Client) DeleteRepoPool(params *DeleteRepoPoolParams, opts ...ClientOption) error {
 	// TODO: Validate the params before sending
 	if params == nil {
-		params = NewGetParams()
+		params = NewDeleteRepoPoolParams()
 	}
 	op := &runtime.ClientOperation{
-		ID:                 "Get",
+		ID:                 "DeleteRepoPool",
+		Method:             "DELETE",
+		PathPattern:        "/repositories/{repoID}/pools/{poolID}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &DeleteRepoPoolReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	_, err := a.transport.Submit(op)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+GetRepo gets repository by ID
+*/
+func (a *Client) GetRepo(params *GetRepoParams, opts ...ClientOption) (*GetRepoOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewGetRepoParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "GetRepo",
 		Method:             "GET",
 		PathPattern:        "/repositories/{repoID}",
 		ProducesMediaTypes: []string{"application/json"},
 		ConsumesMediaTypes: []string{"application/json"},
 		Schemes:            []string{"http"},
 		Params:             params,
-		Reader:             &GetReader{formats: a.formats},
+		Reader:             &GetRepoReader{formats: a.formats},
 		Context:            params.Context,
 		Client:             params.HTTPClient,
 	}
@@ -135,32 +217,143 @@ func (a *Client) Get(params *GetParams, opts ...ClientOption) (*GetOK, error) {
 	if err != nil {
 		return nil, err
 	}
-	success, ok := result.(*GetOK)
+	success, ok := result.(*GetRepoOK)
 	if ok {
 		return success, nil
 	}
 	// unexpected success response
-	unexpectedSuccess := result.(*GetDefault)
+	unexpectedSuccess := result.(*GetRepoDefault)
 	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
 /*
-List lists repositories
+GetRepoPool gets repository pool by ID
 */
-func (a *Client) List(params *ListParams, opts ...ClientOption) (*ListOK, error) {
+func (a *Client) GetRepoPool(params *GetRepoPoolParams, opts ...ClientOption) (*GetRepoPoolOK, error) {
 	// TODO: Validate the params before sending
 	if params == nil {
-		params = NewListParams()
+		params = NewGetRepoPoolParams()
 	}
 	op := &runtime.ClientOperation{
-		ID:                 "List",
+		ID:                 "GetRepoPool",
+		Method:             "GET",
+		PathPattern:        "/repositories/{repoID}/pools/{poolID}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &GetRepoPoolReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*GetRepoPoolOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*GetRepoPoolDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+ListRepoInstances lists repository instances
+*/
+func (a *Client) ListRepoInstances(params *ListRepoInstancesParams, opts ...ClientOption) (*ListRepoInstancesOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewListRepoInstancesParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "ListRepoInstances",
+		Method:             "GET",
+		PathPattern:        "/repositories/{repoID}/instances",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &ListRepoInstancesReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*ListRepoInstancesOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*ListRepoInstancesDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+ListRepoPools lists repository pools
+*/
+func (a *Client) ListRepoPools(params *ListRepoPoolsParams, opts ...ClientOption) (*ListRepoPoolsOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewListRepoPoolsParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "ListRepoPools",
+		Method:             "GET",
+		PathPattern:        "/repositories/{repoID}/pools",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &ListRepoPoolsReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*ListRepoPoolsOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*ListRepoPoolsDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+ListRepos lists repositories
+*/
+func (a *Client) ListRepos(params *ListReposParams, opts ...ClientOption) (*ListReposOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewListReposParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "ListRepos",
 		Method:             "GET",
 		PathPattern:        "/repositories",
 		ProducesMediaTypes: []string{"application/json"},
 		ConsumesMediaTypes: []string{"application/json"},
 		Schemes:            []string{"http"},
 		Params:             params,
-		Reader:             &ListReader{formats: a.formats},
+		Reader:             &ListReposReader{formats: a.formats},
 		Context:            params.Context,
 		Client:             params.HTTPClient,
 	}
@@ -172,12 +365,86 @@ func (a *Client) List(params *ListParams, opts ...ClientOption) (*ListOK, error)
 	if err != nil {
 		return nil, err
 	}
-	success, ok := result.(*ListOK)
+	success, ok := result.(*ListReposOK)
 	if ok {
 		return success, nil
 	}
 	// unexpected success response
-	unexpectedSuccess := result.(*ListDefault)
+	unexpectedSuccess := result.(*ListReposDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+UpdateRepo updates repository with the parameters given
+*/
+func (a *Client) UpdateRepo(params *UpdateRepoParams, opts ...ClientOption) (*UpdateRepoOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewUpdateRepoParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "UpdateRepo",
+		Method:             "PUT",
+		PathPattern:        "/repositories/{repoID}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &UpdateRepoReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*UpdateRepoOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*UpdateRepoDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+UpdateRepoPool updates repository pool with the parameters given
+*/
+func (a *Client) UpdateRepoPool(params *UpdateRepoPoolParams, opts ...ClientOption) (*UpdateRepoPoolOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewUpdateRepoPoolParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "UpdateRepoPool",
+		Method:             "PUT",
+		PathPattern:        "/repositories/{repoID}/pools/{poolID}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &UpdateRepoPoolReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*UpdateRepoPoolOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*UpdateRepoPoolDefault)
 	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
