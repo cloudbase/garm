f96ea1edcffa66efae1b3a52001901c8034d8743
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2023-07-15 14:39:59 +0000

    Add dockerfile and workflow
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,28 @@
-FROM docker.io/golang:alpine
+FROM docker.io/golang:alpine AS builder
+ARG GARM_REF
 
-WORKDIR /root
-USER root
+LABEL stage=builder
 
-RUN apk add musl-dev gcc libtool m4 autoconf g++ make libblkid util-linux-dev git linux-headers mingw-w64-gcc
+RUN apk add musl-dev gcc libtool m4 autoconf g++ make libblkid util-linux-dev git linux-headers
+RUN git config --global --add safe.directory /build
 
-ADD ./scripts/build-static.sh /build-static.sh
-RUN chmod +x /build-static.sh
+ADD . /build/garm
+RUN cd /build/garm && git checkout ${GARM_REF}
+RUN git clone https://github.com/cloudbase/garm-provider-azure /build/garm-provider-azure
+RUN git clone https://github.com/cloudbase/garm-provider-openstack /build/garm-provider-openstack
 
-CMD ["/bin/sh"]
+RUN cd /build/garm && go build -o /bin/garm \
+    -tags osusergo,netgo,sqlite_omit_load_extension \
+    -ldflags "-linkmode external -extldflags '-static' -s -w -X main.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" \
+    /build/garm/cmd/garm
+RUN mkdir -p /opt/garm/providers.d
+RUN cd /build/garm-provider-azure && go build -ldflags="-linkmode external -extldflags '-static' -s -w" -o /opt/garm/providers.d/garm-provider-azure .
+RUN cd /build/garm-provider-openstack && go build -ldflags="-linkmode external -extldflags '-static' -s -w" -o /opt/garm/providers.d/garm-provider-openstack .
+
+FROM scratch
+
+COPY --from=builder /bin/garm /bin/garm
+COPY --from=builder /opt/garm/providers.d/garm-provider-openstack /opt/garm/providers.d/garm-provider-openstack
+COPY --from=builder /opt/garm/providers.d/garm-provider-azure /opt/garm/providers.d/garm-provider-azure
+
+ENTRYPOINT ["/bin/garm", "-config", "/etc/garm/config.toml"]
--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -10,12 +10,34 @@ export CGO_ENABLED=1
 USER_ID=${USER_ID:-$UID}
 USER_GROUP=${USER_GROUP:-$(id -g)}
 
+mkdir -p $BIN_DIR/amd64 $BIN_DIR/arm64
 cd $GARM_SOURCE/cmd/garm
-go build -mod vendor -o $BIN_DIR/garm -tags osusergo,netgo,sqlite_omit_load_extension -ldflags "-linkmode external -extldflags '-static' -s -w -X main.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
-# GOOS=windows CC=x86_64-w64-mingw32-cc go build -mod vendor -o $BIN_DIR/garm.exe -tags osusergo,netgo,sqlite_omit_load_extension -ldflags "-s -w -X main.Version=$(git describe --always --dirty)" .
+go build -mod vendor \
+    -o $BIN_DIR/amd64/garm \
+    -tags osusergo,netgo,sqlite_omit_load_extension \
+    -ldflags "-linkmode external -extldflags '-static' -s -w -X main.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
+CC=aarch64-linux-musl-gcc GOARCH=arm64 go build \
+    -mod vendor \
+    -o $BIN_DIR/arm64/garm \
+    -tags osusergo,netgo,sqlite_omit_load_extension \
+    -ldflags "-linkmode external -extldflags '-static' -s -w -X main.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
+# GOOS=windows CC=x86_64-w64-mingw32-cc go build -mod vendor \
+#     -o $BIN_DIR/amd64/garm.exe \
+#     -tags osusergo,netgo,sqlite_omit_load_extension \
+#     -ldflags "-s -w -X main.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
 
 cd $GARM_SOURCE/cmd/garm-cli
-go build -mod vendor -o $BIN_DIR/garm-cli -tags osusergo,netgo -ldflags "-linkmode external -extldflags '-static' -s -w -X garm/cmd/garm-cli/cmd.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
-# GOOS=windows CGO_ENABLED=0 go build -mod vendor -o $BIN_DIR/garm-cli.exe -ldflags "-s -w -X garm/cmd/garm-cli/cmd.Version=$(git describe --always --dirty)" .
+go build -mod vendor \
+    -o $BIN_DIR/amd64/garm-cli \
+    -tags osusergo,netgo,sqlite_omit_load_extension \
+    -ldflags "-linkmode external -extldflags '-static' -s -w -X garm/cmd/garm-cli/cmd.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
+CC=aarch64-linux-musl-gcc GOARCH=arm64 go build -mod vendor \
+    -o $BIN_DIR/arm64/garm-cli \
+    -tags osusergo,netgo,sqlite_omit_load_extension \
+    -ldflags "-linkmode external -extldflags '-static' -s -w -X garm/cmd/garm-cli/cmd.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
+# GOOS=windows CGO_ENABLED=0 go build -mod vendor \
+#     -o $BIN_DIR/amd64/garm-cli.exe \
+#     -tags osusergo,netgo,sqlite_omit_load_extension \
+#     -ldflags "-s -w -X garm/cmd/garm-cli/cmd.Version=$(git describe --tags --match='v[0-9]*' --dirty --always)" .
 
 chown $USER_ID:$USER_GROUP -R "$BIN_DIR"
