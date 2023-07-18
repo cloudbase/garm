e3275b4dc1fdaf1af6ebf05e04845e5fa9a9c4a4
Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>
2023-07-18 18:48:01 +0300

    Update generated swagger client code
    Signed-off-by: Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>

--- client/metrics_token/metrics_token_client.go
+++ client/metrics_token/metrics_token_client.go
@@ -30,28 +30,28 @@ type ClientOption func(*runtime.ClientOperation)
 
 // ClientService is the interface for Client methods
 type ClientService interface {
-	MetricsToken(params *MetricsTokenParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*MetricsTokenOK, error)
+	GetMetricsToken(params *GetMetricsTokenParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetMetricsTokenOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
 }
 
 /*
-MetricsToken returns a j w t token that can be used to access the metrics endpoint
+GetMetricsToken returns a j w t token that can be used to access the metrics endpoint
 */
-func (a *Client) MetricsToken(params *MetricsTokenParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*MetricsTokenOK, error) {
+func (a *Client) GetMetricsToken(params *GetMetricsTokenParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*GetMetricsTokenOK, error) {
 	// TODO: Validate the params before sending
 	if params == nil {
-		params = NewMetricsTokenParams()
+		params = NewGetMetricsTokenParams()
 	}
 	op := &runtime.ClientOperation{
-		ID:                 "MetricsToken",
+		ID:                 "GetMetricsToken",
 		Method:             "GET",
 		PathPattern:        "/metrics-token",
 		ProducesMediaTypes: []string{"application/json"},
 		ConsumesMediaTypes: []string{"application/json"},
 		Schemes:            []string{"http"},
 		Params:             params,
-		Reader:             &MetricsTokenReader{formats: a.formats},
+		Reader:             &GetMetricsTokenReader{formats: a.formats},
 		AuthInfo:           authInfo,
 		Context:            params.Context,
 		Client:             params.HTTPClient,
@@ -64,13 +64,13 @@ func (a *Client) MetricsToken(params *MetricsTokenParams, authInfo runtime.Clien
 	if err != nil {
 		return nil, err
 	}
-	success, ok := result.(*MetricsTokenOK)
+	success, ok := result.(*GetMetricsTokenOK)
 	if ok {
 		return success, nil
 	}
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
-	msg := fmt.Sprintf("unexpected success response for MetricsToken: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	msg := fmt.Sprintf("unexpected success response for GetMetricsToken: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
