90c954e0e5d10b834fe83369baf47a7b755b881c
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2023-09-24 08:04:00 +0000

    Replace deprecated function call
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

--- util/util.go
+++ util/util.go
@@ -97,7 +97,7 @@ func GithubClient(ctx context.Context, token string, credsDetails params.GithubC
 	)
 	tc := oauth2.NewClient(ctx, ts)
 
-	ghClient, err := github.NewEnterpriseClient(credsDetails.APIBaseURL, credsDetails.UploadBaseURL, tc)
+	ghClient, err := github.NewClient(tc).WithEnterpriseURLs(credsDetails.APIBaseURL, credsDetails.UploadBaseURL)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "fetching github client")
 	}
