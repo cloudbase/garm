5d596aa94c039a657fece7b72614e671ad2495a8
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2023-12-18 16:39:30 +0000

    Copy provider config in garm folder
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

--- test/integration/scripts/setup-garm.sh
+++ test/integration/scripts/setup-garm.sh
@@ -7,8 +7,6 @@ CONTRIB_DIR="$PWD/contrib"
 CONFIG_DIR="$PWD/test/integration/config"
 CONFIG_DIR_PROV="$PWD/test/integration/provider"
 PROVIDER_BIN_DIR="/opt/garm/providers.d/lxd"
-export LXD_PROVIDER_EXECUTABLE="$PROVIDER_BIN_DIR/garm-provider-lxd"
-export LXD_PROVIDER_CONFIG="$CONFIG_DIR/garm-provider-lxd.toml"
 
 if [[ ! -f $BINARIES_DIR/garm ]] || [[ ! -f $BINARIES_DIR/garm-cli ]]; then
     echo "ERROR: Please build GARM binaries first"
@@ -51,6 +49,10 @@ git clone https://github.com/cloudbase/garm-provider-lxd ~/garm-provider-lxd
 pushd ~/garm-provider-lxd
 go build -o $LXD_PROVIDER_EXECUTABLE
 popd
+
+export LXD_PROVIDER_CONFIG="/etc/garm/garm-provider-lxd.toml"
+export LXD_PROVIDER_EXECUTABLE="$PROVIDER_BIN_DIR/garm-provider-lxd"
+sudo cp $CONFIG_DIR/garm-provider-lxd.toml $LXD_PROVIDER_CONFIG
 
 sudo mkdir -p /etc/garm
 cat $CONFIG_DIR/config.toml | envsubst | sudo tee /etc/garm/config.toml
