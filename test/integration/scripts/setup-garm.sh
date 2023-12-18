6c7c5a913f909a9a17a0e13c69a8a3f1153fb8ac
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2023-12-18 16:43:30 +0000

    Define variable before use
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

--- test/integration/scripts/setup-garm.sh
+++ test/integration/scripts/setup-garm.sh
@@ -44,15 +44,15 @@ export DB_PASSPHRASE="$(generate_secret)"
 # Group "adm" is the LXD daemon group as set by the "canonical/setup-lxd" GitHub action.
 sudo useradd --shell /usr/bin/false --system --groups adm --no-create-home garm
 
+export LXD_PROVIDER_EXECUTABLE="$PROVIDER_BIN_DIR/garm-provider-lxd"
+export LXD_PROVIDER_CONFIG="/etc/garm/garm-provider-lxd.toml"
+sudo cp $CONFIG_DIR/garm-provider-lxd.toml $LXD_PROVIDER_CONFIG
+
 sudo mkdir -p $PROVIDER_BIN_DIR
 git clone https://github.com/cloudbase/garm-provider-lxd ~/garm-provider-lxd
 pushd ~/garm-provider-lxd
 go build -o $LXD_PROVIDER_EXECUTABLE
 popd
-
-export LXD_PROVIDER_CONFIG="/etc/garm/garm-provider-lxd.toml"
-export LXD_PROVIDER_EXECUTABLE="$PROVIDER_BIN_DIR/garm-provider-lxd"
-sudo cp $CONFIG_DIR/garm-provider-lxd.toml $LXD_PROVIDER_CONFIG
 
 sudo mkdir -p /etc/garm
 cat $CONFIG_DIR/config.toml | envsubst | sudo tee /etc/garm/config.toml
